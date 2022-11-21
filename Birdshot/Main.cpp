172f0c2a9d6fc200eb0a3dc58124b2e6610a61f2
ZclipseCode <bmichaelbusiness@gmail.com>
2022-11-21 09:57:14 -0600

    tabs

--- Birdshot/Main.cpp
+++ Birdshot/Main.cpp
@@ -8,11 +8,16 @@ int main() {
     std::string name;
     Component inputName = Input(&name, "Input Name Here...");
     auto component = Container::Vertical({
-        inputName
-    });
-
+            inputName
+        });
     //If Name is Greater than Three Characters, Try Again (to Capture Arcade-Like Feel) (Input Validation)
     std::string nameValidation;
+    auto inputNameRenderer = Renderer([&] {
+        return vbox({
+            text(nameValidation) | hcenter,
+            inputName->Render() | hcenter
+            });
+        });
 
     //renderer is like Unity Update()
     auto renderer = Renderer(component, [&] {
@@ -32,16 +37,42 @@ int main() {
             nameValidation = "Welcome, " + name + "!";
         }
 
+        //Button to Transition to Play Screen after Name is Selected
+        auto gameTabRenderer = Renderer([] {
+            return vbox({
+                text("game tab")
+                });
+            });
+
+        int tab_index = 0;
+        std::vector<std::string> tab_entries = {
+            "Input Name", "Play", "Settings"
+        };
+        auto tab_selection =
+            Menu(&tab_entries, &tab_index, MenuOption::HorizontalAnimated());
+        auto tab_content = Container::Tab(
+            {
+                inputNameRenderer,
+                gameTabRenderer
+            },
+            &tab_index);
+
+        auto main_container = Container::Vertical({
+            tab_selection,
+            tab_content,
+            });
+
         return vbox({
-            text("   ___  _        __    __        __ "),
-            text("  / _ )(_)______/ /__ / /  ___  / /_"),
-            text(" / _  / / __/ _  (_-</ _ \\/ _ \\/ __/"),
-            text("/____/_/_/  \\___/___/_//_/\\___/\\__/ "),
-            text(""),
-            text("Created by Brian Michael"),
-            text(""),
-            text(nameValidation),
-            inputName->Render()
+            text("   ___  _        __    __        __ ") | hcenter,
+            text("  / _ )(_)______/ /__ / /  ___  / /_") | hcenter,
+            text(" / _  / / __/ _  (_-</ _ \\/ _ \\/ __/") | hcenter,
+            text("/____/_/_/  \\___/___/_//_/\\___/\\__/ ") | hcenter,
+            text("") | hcenter,
+            text("Created by Brian Michael") | hcenter,
+            text("") | hcenter,
+
+            tab_selection->Render(),
+            tab_content->Render() | flex
         });
     });
 
